4ea6b02f52bd43d4141217a85a0915dcc8eb311e
miriamdaw <114226559+miriamdaw@users.noreply.github.com>
2023-01-16 11:16:10 +0100

    Update Practica442.java

--- src/main/java/com/mycompany/practica442/Practica442.java
+++ src/main/java/com/mycompany/practica442/Practica442.java
@@ -13,5 +13,7 @@ public class Practica442 {
 
     public static void main(String[] args) {
         System.out.println("Esta es mi práctica de Control de Versiones en IDE.");
+        
+        System.out.println("Soy Miriam Ramos González.");
     }
 }
